c7e48077730f0a772b0b8be69a6d7090c7d2f94f
Nabhoneel Majumdar <email@nparchments.com>
2018-01-30 10:02:09 +0530

    Bootstrap updated

---
+++ assist.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Assist Customer</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/assist.css" />
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <script src="./js/jquery.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<div class="row">
+    <div class="col-sm-4"></div>
+    <div class="col-sm-4">
+        <div class="jumbotron">
+            I want to center this div with Bootstrap.
+        </div>
+    </div>
+    <div class="col-sm-4"></div>
+</div>
+</body>
+</html>
