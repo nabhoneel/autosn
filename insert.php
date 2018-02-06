1918aa85104f42956065395123c825152c6f452f
Nabhoneel Majumdar <email@nparchments.com>
2018-02-07 01:35:29 +0530

    wrote 500+ queries

--- insert.php
+++ insert.php
@@ -1,13 +1,26 @@
 <?php
 include 'connection.php';
-$options = array(1, 2, 5);
-$company = "Maruti Suzuki";
-$model = "WagonR";
 
-foreach($options as $i) {
-    $query = "INSERT INTO `model has options` (`company name`, `model name`, `option id`) VALUES ('$company', '$model', '$i')";
-    echo $query."<br>";
-    if($mysqli->query($query) === TRUE) echo "Successful<br>";
-    else echo "Unsuccessful";
+$vins = $mysqli->query("SELECT `index number` FROM `cars`");
+$success = $failure = 0;
+
+foreach($vins as $x) {
+    $options = array(1, 2, 3, 4, 5, 6, 7);
+    $a = 0;
+
+    $vin = $x["index number"];
+    $times = array_rand($options, 1) + 1;
+    while ($a <= $times) {
+        $k = array_rand($options, 1);
+        $option = $options[$k];
+        unset($options[$k]);
+        
+        $query = "INSERT INTO `car has options` (`vehicle index`, `option id`) VALUES ('$vin', '$option')";
+        if($mysqli->query($query) === TRUE) $success++;
+        else $failure++;
+        $a++;
+    }
 }
+
+echo "Success: ".$success."<br>Failure: ".$failure;
 ?>
